da20880520ec83e137baddbb36f10a59441978df
Øystein Håberg <islandstone89@gmail.com>
2024-05-09 01:57:46 +0200

    * Added close menu beutton * Added mobile menu styles * Added toggleMenu function

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 
 /* Select the open menu button */
-let openMenuBtn = document.querySelector(".open-menu-btn");
+let openMenuBtn = document.querySelector(".js-open-menu-btn");
+
+/* Select the close menu button */
+let closeMenuBtn = document.querySelector(".js-close-menu-btn");
+
+/* Add function for toggling the menu */
+function toggleMenu () {
+    if (openMenuBtn.getAttribute("aria-expanded", "false")) {
+        openMenuBtn.setAttribute("aria-expanded", "true")
+    }
+    else {
+        openMenuBtn.setAttribute("aria-expanded", "false")
+    }
+}
+
+/* Add event listener */
+
+openMenuBtn.addEventListener("click", toggleMenu);
+
+closeMenuBtn.addEventListener("click", toggleMenu);
+
